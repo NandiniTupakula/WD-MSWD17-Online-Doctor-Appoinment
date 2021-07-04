967427a495562c7f06b456aacf0883c710d001f4
NandiniTupakula <74001304+NandiniTupakula@users.noreply.github.com>
2021-07-04 12:00:12 +0530

    Add files via upload

---
+++ environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  domain: 'http://localhost:1203/booking/v2/',
+  consumerDomain: 'http://localhost:1203/booking/v2/',
+  colors: {
+    customPrimary: '#20b04b',
+    customDanger: '#ea1d25',
+    customInfo: '#68b3cd',
+  }
+};
---
+++ main.ts
@@ -0,0 +1,16 @@
+import { enableProdMode, LOCALE_ID } from '@angular/core';
+import { platformBrowserDynamic  } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+import 'hammerjs'; // for Gesture Support
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule, {
+  // providers: [{provide: LOCALE_ID, useValue: 'en-US' }]
+})
+.catch(err => console.error(err));
